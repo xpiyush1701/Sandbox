87c574a010885bbfcfb63aaaea3a369f9b623afa
Piyush Verma <piyush.verma@my.jcu.edu.au>
2023-09-25 21:01:30 +1000

    Add password check program

--- password_stars.py
+++ password_stars.py
@@ -3,7 +3,7 @@ min_length = 8
 while True:
     password = input("Enter a password: ")
     if len(password) < min_length:
-        print(f"Password must be at least {min_length} characters long.")
+        print(f"Password must be at least {min_length} characters long!")
     else:
         print("*" * len(password))
         break
